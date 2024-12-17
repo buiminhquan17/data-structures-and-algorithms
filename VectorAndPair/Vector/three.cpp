58b1342a444407c74f0e48d1580b3dc36a20926c
buiminhquan17 <hellobuiminhquan@gmail.com>
2024-12-17 17:19:08 +0700

    giai bai ba

---
+++ VectorAndPair/Vector/three.cpp
@@ -0,0 +1,26 @@
+/*
+	Cho vector V có N phần tử, nhiệm vụ của bạn là sắp xếp các phần tử trong vector theo thứ tự tăng dần, giảm dần sau đó in ra bằng cách dùng iterator.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(void) {
+	int length;
+	std::cin >> length;
+	std::vector<int> vector(length);
+	for (auto& element : vector) {
+		std::cin >> element;
+	}
+	std::sort(vector.begin(), vector.end());
+	for (auto it = vector.begin(); it != vector.end(); ++it) {
+		std::cout << (*it) << ' ';
+	}
+	std::cout << '\n';
+	std::sort(vector.begin(), vector.end(), std::greater<int>());
+	for (auto it = vector.begin(); it != vector.end(); ++it) {
+		std::cout << (*it) << ' ';
+	}
+	return 0;
+}
